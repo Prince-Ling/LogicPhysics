7bd97688ef07372899aa34892411f56be93637a5
Prince-Ling <tj134679258@163.com>
2017-09-09 12:07:31 +0800

    某某荣耀 定点数 与 逻辑碰撞

---
+++ Assets/Src/DemoSrc/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public Collider collider1;
+    public Collider collider2;
+
+    private VCollisionShape vCollider1;
+    private VCollisionShape vCollider2;
+    // Use this for initialization
+    void Start () {
+        vCollider1 = VCollisionShape.createFromCollider(collider1.gameObject);
+        vCollider2 = VCollisionShape.createFromCollider(collider2.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        vCollider1.UpdateShape((VInt3)collider1.transform.position, (VInt3)collider1.transform.forward);
+        vCollider2.UpdateShape((VInt3)collider2.transform.position, (VInt3)collider2.transform.forward);
+        if(vCollider1.Intersects(vCollider2))
+        {
+            Debug.Log("true");
+
+        }
+
+        if (vCollider1.EdgeIntersects(vCollider2))
+        {
+            Debug.Log("true22");
+
+        }
+    }
+}
